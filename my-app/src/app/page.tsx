47f16346f21a04eb3fad1a358d756e079ee341a3
saitadikonda99 <saitadikonda.tech@gmail.com>
2025-10-28 14:32:15 +0530

    feat(UI): Implemented Authentication UI

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+      <div className="HomeComponent">
+        <div className="HomeComponent-in">
+          <h1>Heyy</h1>
+        </div>
+      </div>
+  );
+}
